b82d03a2c18476a39a5817415465e8df96b8fc84
lfglfernando <lfglfernandog@gmail.com>
2024-07-15 17:54:03 -0600

    changes

--- project/scripts/siteplan.js
+++ project/scripts/siteplan.js
@@ -4,106 +4,90 @@ const projects = [
       languages: "HTML, CSS, JavaScript",
       date: "Nov, 2023",
       imageUrl:
-      "images/marriage-proposal.webp"
+      "images/marriage-proposal.webp",
+      projectLink: "https://lfglfernando.github.io/wdd130/proposal/netflix.html"
     },
     {
       projectName: "Temple List",
       languages: "HTML, CSS, JavaScript",
       date: "July, 2024",
       imageUrl:
-      "images/temple-list.webp"
+      "images/temple-list.webp",
+      projectLink: "https://lfglfernando.github.io/wdd131/filtered-temples.html"
     },
     {
         projectName: "Manti Utah",
         languages: "Manti, Utah, United States",
         date: "1888, May, 21",
         imageUrl:
-        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg",
+        projectLink: "https://lfglfernando.github.io/wdd130/proposal/netflix.html"
+
     },
     {
         projectName: "Manti Utah",
         languages: "Manti, Utah, United States",
         date: "1888, May, 21",
         imageUrl:
-        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg",
+        projectLink: "https://lfglfernando.github.io/wdd130/proposal/netflix.html"
+
     },
     {
         projectName: "Manti Utah",
         languages: "Manti, Utah, United States",
         date: "1888, May, 21",
         imageUrl:
-        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg",
+        projectLink: "https://lfglfernando.github.io/wdd130/proposal/netflix.html"
+
     },
     {
         projectName: "Manti Utah",
         languages: "Manti, Utah, United States",
         date: "1888, May, 21",
         imageUrl:
-        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+        "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg",
+        projectLink: "https://lfglfernando.github.io/wdd130/proposal/netflix.html"
+
     }
   ];
 
-  function createTempleCard(project) {
+  function createProjectCard(project) {
     const container = document.querySelector('.grid-container');
 
     const figure = document.createElement('figure');
     figure.classList.add('temple-card');
 
+    const a = document.createElement('a');
+    a.classList.add("card-a");
+    a.href = project.projectLink;
+    a.target = "_blank"
+    figure.appendChild(a);
+
     const title = document.createElement('h2');
     title.textContent = project.projectName;
-    figure.appendChild(title);
+    a.appendChild(title);
 
     const language = document.createElement('p');
     language.innerHTML = `<span class="label">Languagues:</span> ${project.languages}`;
-    figure.appendChild(language);
+    a.appendChild(language);
 
     const dedicated = document.createElement('p');
     dedicated.innerHTML = `<span class="label">Date:</span> ${project.date}`;
-    figure.appendChild(dedicated);
+    a.appendChild(dedicated);
 
     const img = document.createElement('img');
     img.src = project.imageUrl;
     img.alt = project.projectName;
     img.loading = 'lazy';
-    figure.appendChild(img);
+    a.appendChild(img);
 
     container.appendChild(figure);
 }
 
-  projects.forEach(createTempleCard);
+  projects.forEach(createProjectCard);
 
-  document.querySelector('nav').addEventListener('click', function(event) {
-    if (event.target.tagName === 'A') {
-        const filter = event.target.textContent.toLowerCase();
-        const container = document.querySelector('.grid-container');
-        container.innerHTML = ''; //Clear existing content
-
-        let filteredTemples;
-
-        switch (filter) {
-            case 'home':
-                filteredTemples = projects;
-                break;
-            case 'old':
-                filteredTemples = projects.filter(temple => new Date(temple.dedicated).getFullYear() < 1900);
-                break;
-            case 'new':
-                filteredTemples = projects.filter(temple => new Date(temple.dedicated).getFullYear() > 2000);
-                break;
-            case 'large':
-                filteredTemples = projects.filter(temple => temple.area > 90000);
-                break;
-            case 'small':
-                filteredTemples = projects.filter(temple => temple.area < 10000);
-                break;
-            default:
-                filteredTemples = projects;
-
-        }
-
-        filteredTemples.forEach(createTempleCard);
-    }
-  });
 
 const hamButton = document.querySelector('#menu');
 const navigation = document.querySelector('.navigation');
